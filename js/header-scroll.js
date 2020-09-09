432c714add251a2fc01d8e1fd59c6feaab049a6b
alexuniverses <bamboxspace@gmail.com>
2020-09-09 23:36:41 +0300

    Добавил плавный переход по якорям на всех страницах

--- js/header-scroll.js
+++ js/header-scroll.js
@@ -4,8 +4,9 @@ window.onscroll = function () { scrollFunction() };
       if (document.body.scrollTop > 88 || document.documentElement.scrollTop > 88) {
         document.getElementById("header_nav").style.padding = "0px 0px";
         document.getElementById("header_nav").style.background="#281001";
-      } else {
-        document.getElementById("header_nav").style.padding = "108px 0px";
+      } 
+      else {
+        document.getElementById("header_nav").style.padding = "90px 0px";
         document.getElementById("header_nav").style.background="transparent";
       }
     }
--- js/js_compile/header-scroll.dev.js
+++ js/js_compile/header-scroll.dev.js
@@ -9,7 +9,7 @@ function scrollFunction() {
     document.getElementById("header_nav").style.padding = "0px 0px";
     document.getElementById("header_nav").style.background = "#281001";
   } else {
-    document.getElementById("header_nav").style.padding = "108px 0px";
+    document.getElementById("header_nav").style.padding = "90px 0px";
     document.getElementById("header_nav").style.background = "transparent";
   }
 }
